a2059dcf2c866ba2eb39dc939ea322d9c5e76b0c
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-28 16:52:28 -0300

    first commit

---
+++ src/main/java/br/com/gbruno/loginappauth/LoginAppAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbruno.loginappauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAppAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gbruno/loginappauth/LoginAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbruno.loginappauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
