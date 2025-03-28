0fe0c889f43f1ecbc830227b6d297fc51a65772d
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-28 17:12:44 -0300

    feat: Created entity that represent the User

--- src/main/java/br/com/gbruno/loginappauth/domain/user/User.java
+++ src/main/java/br/com/gbruno/loginappauth/domain/user/User.java
@@ -1,4 +1,26 @@
 package br.com.gbruno.loginappauth.domain.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
 }
