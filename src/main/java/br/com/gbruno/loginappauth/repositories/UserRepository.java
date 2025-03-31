8e14d4f5777118ae88425378263f5bccac69a3d4
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-31 17:46:57 -0300

    feat: Criado SecurityFilter que irá ser o filtro de segurança

--- src/main/java/br/com/gbruno/loginappauth/repositories/UserRepository.java
+++ src/main/java/br/com/gbruno/loginappauth/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package br.com.gbruno.loginappauth.repositories;
 import br.com.gbruno.loginappauth.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
