2f77b967b9b5c4cf6d2bf0956ece192e751096bd
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-04-01 20:31:06 -0300

    fix: anotação GetMapping

--- src/main/java/br/com/gbruno/loginappauth/controllers/UserController.java
+++ src/main/java/br/com/gbruno/loginappauth/controllers/UserController.java
@@ -1,6 +1,7 @@
 package br.com.gbruno.loginappauth.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  @RequestMapping("/users")
 public class UserController {
 
+    @GetMapping
     public ResponseEntity<String> getUser() {
         return ResponseEntity.ok("Sucess");
     }
